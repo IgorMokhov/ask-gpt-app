3c2c9c519bbb59b37fb9a19ca7f212e5776a9bc2
Igor Mokhov <igormokhovid@gmail.com>
2025-05-09 16:16:44 +0700

    feat: added WelcomeMessage

---
+++ client/src/components/WelcomeMessage/WelcomeMessage.tsx
@@ -0,0 +1,11 @@
+export const WelcomeMessage = () => {
+  return (
+    <div className="flex flex-col gap-11 mt-30">
+      <p className="text-8xl">Hi there!</p>
+      <p className="text-6xl">What would you like to know?</p>
+      <p className="max-w-150 text-3xl text-gray-400">
+        Use one of the most common propmpts below or ask your own question
+      </p>
+    </div>
+  );
+};
