702764f85ec196afcbb146e96303fbfa94d5e82c
Igor Mokhov <igormokhovid@gmail.com>
2025-05-09 14:24:47 +0700

    feat: added Container

---
+++ client/src/UI/Container/Container.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react';
+
+interface IContainerProps {
+  children: ReactNode;
+}
+
+export const Container = ({ children }: IContainerProps) => {
+  return <div className={'max-w-[1400px] mx-auto px-4'}>{children}</div>;
+};
