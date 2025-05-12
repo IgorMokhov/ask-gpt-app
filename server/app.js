505d8222da005d0ae527c10afa9e28b93e0f1606
Igor Mokhov <igormokhovid@gmail.com>
2025-05-12 14:32:57 +0700

    feat: added initial Express server (app.js)

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import { getMessageHandler } from './controllers/messages.js';
 
+const PORT = 5001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post('/messages', getMessageHandler);
+
+app.listen(PORT, () => console.log(`Server was started on port ${PORT}`));
